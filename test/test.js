1c2baabd5d7af399a5dfcdda3d1a7817c26ae82d
Lee Byron <lee@leebyron.com>
2016-07-24 14:18:04 -0700

    Better test update script

--- test/test.js
+++ test/test.js
@@ -5,9 +5,5 @@ var flowRemoveTypes = require('../');
 var source = fs.readFileSync(__dirname + '/source.js', 'utf8');
 var actual = flowRemoveTypes(source);
 
-if (process.argv[2] === '--update') {
-  fs.writeFileSync(__dirname + '/expected.js', actual);
-} else {
-  var expected = fs.readFileSync(__dirname + '/expected.js', 'utf8');
-  assert.equal(actual, expected);
-}
+var expected = fs.readFileSync(__dirname + '/expected.js', 'utf8');
+assert.equal(actual, expected);
