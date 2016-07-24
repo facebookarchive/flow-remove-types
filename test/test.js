4b6d230a494175035bc90abd22ef4988ef4b71ee
Lee Byron <lee@leebyron.com>
2016-07-24 14:53:26 -0700

    Replace test with simple shell script

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var fs = require('fs');
-var flowRemoveTypes = require('../');
-
-var source = fs.readFileSync(__dirname + '/source.js', 'utf8');
-var actual = flowRemoveTypes(source);
-
-var expected = fs.readFileSync(__dirname + '/expected.js', 'utf8');
-assert.equal(actual, expected);
