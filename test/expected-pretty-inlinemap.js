1827549c7f9bad088635ef0bff66cd823e331d27
Lee Byron <lee@leebyron.com>
2017-01-26 13:00:07 -0800

    Remove trailing new lines in pretty mode. (#31)
    Fixes #28

--- test/expected-pretty-inlinemap.js
+++ test/expected-pretty-inlinemap.js
@@ -5,7 +5,6 @@ import { Something } from 'some-module';
 
 // Import types
 
-
 // Typed function
 async function test(x, y /*.*/  /*.*/ , z /*.*/  /*.*/  = 123) {
   // Typed expression
@@ -14,12 +13,9 @@ async function test(x, y /*.*/  /*.*/ , z /*.*/  /*.*/  = 123) {
 
 // Interface
 
-
 // Exported interface
 
-
 // Interface extends
-
 
 // Implements interface
 class Bar extends Other  /*.*/   {
@@ -28,7 +24,6 @@ class Bar extends Other  /*.*/   {
 
   // Class Property
   
-
   method() {
     return;
   }
@@ -37,7 +32,6 @@ class Bar extends Other  /*.*/   {
 // Class expression implements interface
 var SomeClass = class Baz   {
   
-
   method() {
     return;
   }
@@ -61,33 +55,24 @@ class StringWrapper extends Wrapper {
 
 // Declare class
 
-
 // Declare funtion
-
 
 // Declare interface
 
-
 // Declare module
-
 
 // Declare type alias
 
-
 // Declare variable
-
 
 // Type alias
 
-
 // Export type
-
 
 // Regular export
 export { Wrapper };
 
 // Exported type alias
-
 
 // Object with types within
 var someObj = {
@@ -99,11 +84,9 @@ var someObj = {
 // Example from README
 import SomeClass from 'some-module'
 
-
 export class MyClass extends SomeClass   {
 
   
-
   constructor(value) {
     this.value = value
   }
@@ -114,4 +97,4 @@ export class MyClass extends SomeClass   {
 
 }
 
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3Qvc291cmNlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJHQUFHLEFBQUs7Ozs7OztBQU1SLEFBQTRDOzs7cUJBR3ZCLEFBQU0sVUFBVSxBQUFDLGlCQUFpQixBQUFDLE9BQU8sQUFBYyxPQUFPLEFBQVE7O2lCQUUzRSxBQUFLOzs7O0FBSXRCLEFBSUM7OztBQUdELEFBRUM7OztBQUdELEFBRUM7Ozt3QkFHdUIsQUFBVSxPQUFPLEFBQUcsQUFBQyxDQUFDLEFBQVU7O1FBRWhELEFBQVE7OztFQUdkLEFBQVU7O1VBRUYsQUFBTzs7Ozs7OzBCQU1TLEFBQVUsQ0FBQyxBQUFHO0VBQ3RDLEFBQVU7O1VBRUYsQUFBTzs7Ozs7O2FBTUosQUFBRztPQUNULEFBQUc7Ozs7S0FJTCxBQUFHLEVBQUUsQUFBWTs7Ozs7O21DQU1hLEFBQVE7Ozs7O0FBSzNDLEFBRUM7OztBQUdELEFBQWtDOzs7QUFHbEMsQUFFQzs7O0FBR0QsQUFFQzs7O0FBR0QsQUFHRTs7O0FBR0YsQUFBK0I7OztBQUcvQixBQUFnQjs7O0FBR2hCLEFBQWtCOzs7Ozs7QUFNbEIsQUFBa0M7Ozs7YUFJckIsQUFBTTs7Ozs7OztBQU9uQixBQUFnRDs7b0JBRTVCLEFBQUcsbUJBQW1CLEFBQVUsQ0FBQyxBQUFhOztFQUVoRSxBQUFROzttQkFFUyxBQUFHOzs7O09BSWYsQUFBRyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3Qvc291cmNlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJHQUFHLEFBQUs7Ozs7OztBQU1SLEFBQTRDLEFBQUE7OztxQkFHdkIsQUFBTSxVQUFVLEFBQUMsaUJBQWlCLEFBQUMsT0FBTyxBQUFjLE9BQU8sQUFBUTs7aUJBRTNFLEFBQUs7Ozs7QUFJdEIsQUFJQyxBQUFBOzs7QUFHRCxBQUVDLEFBQUE7OztBQUdELEFBRUMsQUFBQTs7O3dCQUd1QixBQUFVLE9BQU8sQUFBRyxBQUFDLENBQUMsQUFBVTs7UUFFaEQsQUFBUTs7O0VBR2QsQUFBVSxBQUFBOztVQUVGLEFBQU87Ozs7OzswQkFNUyxBQUFVLENBQUMsQUFBRztFQUN0QyxBQUFVLEFBQUE7O1VBRUYsQUFBTzs7Ozs7O2FBTUosQUFBRztPQUNULEFBQUc7Ozs7S0FJTCxBQUFHLEVBQUUsQUFBWTs7Ozs7O21DQU1hLEFBQVE7Ozs7O0FBSzNDLEFBRUMsQUFBQTs7O0FBR0QsQUFBa0MsQUFBQTs7O0FBR2xDLEFBRUMsQUFBQTs7O0FBR0QsQUFFQyxBQUFBOzs7QUFHRCxBQUdFLEFBQUE7OztBQUdGLEFBQStCLEFBQUE7OztBQUcvQixBQUFnQixBQUFBOzs7QUFHaEIsQUFBa0IsQUFBQTs7Ozs7O0FBTWxCLEFBQWtDLEFBQUE7Ozs7YUFJckIsQUFBTTs7Ozs7OztBQU9uQixBQUFnRCxBQUFBOztvQkFFNUIsQUFBRyxtQkFBbUIsQUFBVSxDQUFDLEFBQWE7O0VBRWhFLEFBQVEsQUFBQTs7bUJBRVMsQUFBRzs7OztPQUlmLEFBQUcifQ==
