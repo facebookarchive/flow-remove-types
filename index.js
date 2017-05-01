975c67bdbab9f2ef2cfd5817e4bb4e50b36d7b3d
Laurence Rowe <l@lrowe.co.uk>
2017-05-01 12:14:28 -0700

    Handle array destructuring assignment with ignored values (#36)
    Avoid `TypeError: Cannot read property 'type' of null` triggered by parse of array destructuring assignment with ignored values, e.g. `const [, y] = pair;`

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ function visit(ast, context, visitor) {
       stack = stack.prev;
     } else {
       var node = parent ? parent[keys[index]] : ast.program;
-      if (node && typeof node === 'object' && (node.type || node.length && node[0].type)) {
+      if (node && typeof node === 'object' && (node.type || node.length)) {
         if (node.type) {
           var visitFn = visitor[node.type];
           if (visitFn && visitFn(context, node, ast) === false) {
